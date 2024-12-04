c350e3b119f7c687fa1308cfbfeaf1f376bcf6dd
toluooshy <56281958+toluooshy@users.noreply.github.com>
2024-12-04 01:15:42 -0500

    GUI for parameter sweep script.

---
+++ scripts/filtering_script_125.sh
@@ -0,0 +1,10 @@
+conda activate py27
+conda activate rad-env
+cd /data/working_env
+/usr/local/bin/vcftools --vcf (ORIGINAL VCF)--out _filtered_min10x_mm_0.5_m_0.5_maf_0.05 --min-meanDP 10 --max-missing 0.5 --recode
+/usr/local/bin/vcftools --vcf (VCF FROM 1st FUNCTION) --out (LIST OF SINGLETONS) --singletons
+/usr/local/bin/vcftools --vcf (VCF FROM 1st FUNCTION) --out (FILE WITHOUT SINGLETONS) --exclude-positions(LIST OF SINGLETONS) --recode
+/usr/local/bin/vcftools --vcf _filtered_min10x_final.recode.vcf --out _missing_mm_0.5_m_0.5_maf_0.05 --missing-indv
+awk <_missing.imiss ' $5>0.5 <built-in function print> ' >_to_exclude.txt
+/usr/local/bin/vcftools --vcf _filtered_min10x_final.recode.vcf --out _vcffilter --remove_to_exclude.txt --recode
+/usr/local/bin/vcftools --vcf _vcf.recode.vcf --out _final_vcffilter_mm_0.5_m_0.5_maf_0.05 --maf 0.05
